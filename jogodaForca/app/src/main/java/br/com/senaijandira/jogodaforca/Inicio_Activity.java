6530d11e1f0d36ac04751a9d3958a1e15fcea35a
MIKAELA GARCIA DE OLIVEIRA ALENCAR <17259222@CFP127ALUNO.LOCAL>
2018-08-27 11:18:08 -0300

    Jogo da Forca desenvolvido em aula utilizando Android

---
+++ jogodaForca/app/src/main/java/br/com/senaijandira/jogodaforca/Inicio_Activity.java
@@ -0,0 +1,28 @@
+package br.com.senaijandira.jogodaforca;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by 17259222 on 27/08/2018.
+ */
+
+public class Inicio_Activity extends Activity {
+
+    ImageView img_forca;
+    Button  btn_inicio;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_activity);
+
+//
+
+
+
+    }
+}
---
+++ jogodaForca/app/src/main/java/br/com/senaijandira/jogodaforca/Main_Activity.java
@@ -0,0 +1,10 @@
+package br.com.senaijandira.jogodaforca;
+
+import android.app.Activity;
+
+/**
+ * Created by 17259222 on 27/08/2018.
+ */
+
+public class Main_Activity extends Activity {
+}
